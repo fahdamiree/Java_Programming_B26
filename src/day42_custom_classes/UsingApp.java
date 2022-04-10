a37320d3c35626a5ee6cb430bea215f37eeed42d
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-09 22:39:16 -0700

    Class and Object Example:

---
+++ src/day42_custom_classes/UsingApp.java
@@ -0,0 +1,48 @@
+package day42_custom_classes;
+
+import day34_methods.ScannerNumberWords;
+
+import java.util.Scanner;
+
+public class UsingApp {
+    public static void main(String[] args) {
+
+        App cydeo = new App();
+
+        cydeo.name = "Cydeo";
+        cydeo.version = 5.5;
+        cydeo.isFree = true;
+
+        // we cannot call instance methods like this:
+//        App.run();
+//        App.update();
+
+        cydeo.run();
+        cydeo.update();
+
+        System.out.println(cydeo.name);
+        System.out.println(cydeo.version);
+        System.out.println(cydeo.isFree);
+        System.out.println(cydeo);
+
+        new App().run();// this creates an object with no reference and calls the run method
+
+        System.out.println(new Scanner(System.in).nextLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
